53e48dce6de4e1760331f2c949d435727cd18b87
Weslley Victor <wvictor07@gmail.com>
2021-08-15 20:13:27 +0100

    Refactored bad code on Trailer.js component

--- frontend/src/components/movieContainer/Trailer.js
+++ frontend/src/components/movieContainer/Trailer.js
@@ -3,16 +3,17 @@ import {FacebookShareButton, FacebookIcon, TwitterShareButton, TwitterIcon, Tele
 
 class Trailer extends Component {
   render() { 
+    const url = "https://youtube.com/watch?v="+this.props.youtubeId;
     return (
       <div style={{marginBottom: '50px'}}>
         <h1>Trailer</h1>
         <iframe style={{border: 'none'}} title={this.props.youtubeId} width='80%' height='500px'  src={`https://youtube.com/embed/${this.props.youtubeId}`}></iframe>
         <div className='mt-3'>
           <p>Share trailer with your friends: </p>
-          <FacebookShareButton className="ml-3 mr-3" children={<FacebookIcon size={32} round={true} />} url={"https://youtube.com/watch?v="+this.props.youtubeId} />
-          <TwitterShareButton className="ml-3 mr-3" children={<TwitterIcon size={32} round={true} />} url={"https://youtube.com/watch?v="+this.props.youtubeId} />
-          <TelegramShareButton className="ml-3 mr-3" children={<TelegramIcon size={32} round={true} />} url={"https://youtube.com/watch?v="+this.props.youtubeId} />
-          <WhatsappShareButton className="ml-3 mr-3" children={<WhatsappIcon size={32} round={true} />} url={"https://youtube.com/watch?v="+this.props.youtubeId} />
+          <FacebookShareButton className="ml-3 mr-3" children={<FacebookIcon size={32} round={true} />} url={url} />
+          <TwitterShareButton className="ml-3 mr-3" children={<TwitterIcon size={32} round={true} />} url={url} />
+          <TelegramShareButton className="ml-3 mr-3" children={<TelegramIcon size={32} round={true} />} url={url} />
+          <WhatsappShareButton className="ml-3 mr-3" children={<WhatsappIcon size={32} round={true} />} url={url} />
         </div>
       </div>
     );
