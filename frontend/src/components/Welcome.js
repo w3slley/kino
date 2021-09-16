4c499bfc792f07598694e122a63a22553c7f6a3f
Weslley Victor <wvictor07@gmail.com>
2021-09-16 11:19:46 +0100

    Changed welcome button text

--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -14,7 +14,7 @@ class Welcome extends Component {
           <h1 className="jumbotron-heading">Welcome to Kino</h1>
           <p className="lead text-muted">The right place to learn more about the movies you love!</p>
           <p>
-            <Link to="/search" className="btn btn-secondary my-2">Search movies</Link>
+            <Link to="/search" className="btn btn-secondary my-2">Search for movies</Link>
           </p>
         </div>
       </section>
