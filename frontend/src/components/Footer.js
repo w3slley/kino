1b433fc3bed291f5702ec244f3089402127cc0ed
Weslley <wvictor07@gmail.com>
2023-04-15 01:52:46 +0100

    Add year dynamically

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,11 +1,14 @@
 import {Component} from 'react';
 
 class Footer extends Component {
-  state = {  }
+  currentYear = new Date().getFullYear();
+  
   render() { 
     return (
       <footer style={{display:'flex',alignItems:'center',justifyContent:'center',height:50,margin:'20px 0 0 0',backgroundColor: '#262626',position:'absolute',bottom:0,width:'100%'}}>
-        <p style={{margin:0,color: 'white'}}>Made by <a target='_blank' rel="noreferrer" href='https://w3slley.github.io'>w3slley</a>. © 2021</p>
+        <p style={{margin:0,color: 'white'}}>
+          Made by <a target='_blank' rel="noreferrer" href='https://w3slley.github.io'>w3slley</a>. © {this.currentYear}
+        </p>
       </footer>
     );
   }
